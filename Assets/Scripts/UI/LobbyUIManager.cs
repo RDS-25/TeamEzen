73a7cf87559ca9f4682c9a3a61615fe700e7a2ff
Gkrbs <martian97@naver.com>
2023-08-25 12:41:54 +0900

    씬 수정

--- Assets/Scripts/UI/LobbyUIManager.cs
+++ Assets/Scripts/UI/LobbyUIManager.cs
@@ -3,36 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-/* 20230814
- * 각각의 활성화 버튼 함수 작성
- */
+
 public class LobbyUIManager : MonoBehaviour
 {
     // 메인 로비 UI 버튼
+    public GameObject gLobbyPanel;
     public GameObject gSettingPanel;
+    public GameObject gInvenPanel;
+    public GameObject gSelectStagePanel;
+    public GameObject gSelectCharacterPanel;
+    public GameObject gSkillPanel;
 
+    private void Start()
+    {
+        ButtonLobby();
+    }
     // 설정 버튼
     public void ButtonSetting()
     {
+        gLobbyPanel.SetActive(false);
         gSettingPanel.SetActive(true);
+        gInvenPanel.SetActive(false);
+        gSelectStagePanel.SetActive(false);
+        gSelectCharacterPanel.SetActive(false);
+        gSkillPanel.SetActive(false);
     }
     // 인벤토리 버튼
     public void ButtonInventory()
     {
-
+        gLobbyPanel.SetActive(false);
+        gSettingPanel.SetActive(false);
+        gInvenPanel.SetActive(true);
+        gSelectStagePanel.SetActive(false);
+        gSelectCharacterPanel.SetActive(false);
+        gSkillPanel.SetActive(false);
     }
     // 스테이지 버튼
-    public void ButtonStage()
+    public void ButtonSelectStage()
     {
+        gLobbyPanel.SetActive(false);
+        gSettingPanel.SetActive(false);
+        gInvenPanel.SetActive(false);
+        gSelectStagePanel.SetActive(true);
+        gSelectCharacterPanel.SetActive(false);
+        gSkillPanel.SetActive(false);
     }
     // 캐릭터 버튼
     public void ButtonCharacter()
     {
-
+        gLobbyPanel.SetActive(false);
+        gSettingPanel.SetActive(false);
+        gInvenPanel.SetActive(false);
+        gSelectStagePanel.SetActive(false);
+        gSelectCharacterPanel.SetActive(true);
+        gSkillPanel.SetActive(false);
     }
     // 스킬 버튼
     public void ButtonSkill()
     {
-
+        gLobbyPanel.SetActive(false);
+        gSettingPanel.SetActive(false);
+        gInvenPanel.SetActive(false);
+        gSelectStagePanel.SetActive(false);
+        gSelectCharacterPanel.SetActive(false);
+        gSkillPanel.SetActive(true);
+    }
+    public void ButtonLobby()
+    {
+        gLobbyPanel.SetActive(true);
+        gSettingPanel.SetActive(false);
+        gInvenPanel.SetActive(false);
+        gSelectStagePanel.SetActive(false);
+        gSelectCharacterPanel.SetActive(false);
+        gSkillPanel.SetActive(false);
     }
 }
