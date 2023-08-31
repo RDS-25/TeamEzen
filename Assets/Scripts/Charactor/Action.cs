ba635e6d3d1ef566334cb81c9415a0840052b0dd
ruddnjs1289 <71740497+ruddnjs1289@users.noreply.github.com>
2023-08-31 15:10:35 +0900

    상세보기 할때  안움직이게 하기
    상세보기

--- Assets/Scripts/Charactor/Action.cs
+++ Assets/Scripts/Charactor/Action.cs
@@ -10,6 +10,10 @@ public class Action : MonoBehaviour
     Vector3 Vmove;
 
     Animator ani;
+
+    //입장 했는가 ?
+    bool isEntries = true;
+
     void Start()
     {
         ani = GetComponent<Animator>();   
@@ -18,24 +22,33 @@ public class Action : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (isEntries) {
+            onMove();
+        }
+        if (isEntries) {
+            transform.GetChild(4).gameObject.SetActive(true);
+        }
+
+        if (Input.GetMouseButtonDown(0) && isEntries)
+        {
+            ani.SetBool("isAim", true);
+        }
+        else if (Input.GetMouseButtonUp(0) && isEntries) {
+            ani.SetBool("isAim", false);
+        }
+    }
+
+    void onMove() {
         fhori = Input.GetAxisRaw("Horizontal");
         fverti = Input.GetAxisRaw("Vertical");
 
         Vmove = new Vector3(fhori, 0, fverti).normalized;
 
-        transform.position += Vmove * 5f*Time.deltaTime;
+        transform.position += Vmove * 5f * Time.deltaTime;
 
         ani.SetBool("isRun", Vmove != Vector3.zero);
 
-        transform.LookAt(transform.position+Vmove);
-
-        if (Input.GetMouseButtonDown(0))
-        {
-
-            ani.SetBool("isAim", true);
-        }
-        else if (Input.GetMouseButtonUp(0)) {
-            ani.SetBool("isAim", false);
-        }
+        transform.LookAt(transform.position + Vmove);
     }
 }
