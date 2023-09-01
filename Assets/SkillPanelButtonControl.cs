f5c06ffa54fdb2e205f5dc6d51bb57fda4318d47
Gkrbs <martian97@naver.com>
2023-09-01 11:46:02 +0900

    SkillPanelUpgrade

--- Assets/SkillPanelButtonControl.cs
+++ Assets/SkillPanelButtonControl.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class SkillPanelButtonControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public SelectCharactorUIManager charactorUIManager;
+    public Skilldatas scriptable;
     void Start()
     {
-        
+        //GameObject s = charactorUIManager.OwnChar[charactorUIManager.curCharID];
+
+        //foreach(SkillParameter.SkilParams skill in scriptable.Skills)
+        //{
+        //    if(skill.fId == s.GetComponent<Stat>().skill1)
+        //    {
+        //        SkillParameter.SkilParams myPassive = scriptable.Skills[];
+        //    }
+        //}
     }
 
     // Update is called once per frame
