e2d5013c867fcaec2e3eb5bf7d900dc57c8112bc
elejs124 <83459302+elejs124@users.noreply.github.com>
2023-09-01 14:02:08 +0900

    Jung
    merge

---
+++ Scripts/RandomList.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RandomL
+{
+    public class RandomList
+    {
+        private static RandomList _instance = null;
+        public static RandomList Inistance
+        {
+            get 
+            {
+                if (_instance == null)
+                {
+                    _instance = new RandomList();
+                }
+
+                return _instance;
+            }
+        }
+
+        //중복 포함
+        public List<int> DuplicateRandomList(int minNum , int maxNum , int maxCount)
+        {
+            List<int> value = new List<int>();
+            for (int i = 0; i < maxCount; i++)
+            {
+                value.Add(Random.Range(minNum, maxNum));
+            }
+
+            return value;
+        }
+
+        //중복 제거
+        public List<int> NotDuplicatedRandomList(int minNum, int maxNum, int maxCount)
+        {
+            List<int> value = new List<int>();
+            for (int i = 0; i < maxCount; i++)
+            {
+                int num = Random.Range(minNum, maxNum);
+                if (value.Contains(num) == false)
+                {
+                    value.Add(num);
+                }
+                else
+                    i--;
+            }
+            return value;
+        }
+    }
+}
