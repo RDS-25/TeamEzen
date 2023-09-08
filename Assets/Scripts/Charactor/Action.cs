8fec92c2a035ac87deddd5c3055dbbc0eacb8d54
ruddnjs1289 <71740497+ruddnjs1289@users.noreply.github.com>
2023-09-08 12:55:11 +0900

    스킬 취소

--- Assets/Scripts/Charactor/Action.cs
+++ Assets/Scripts/Charactor/Action.cs
@@ -6,20 +6,27 @@ public class Action : MonoBehaviour
 {
     float fverti;
     float fhori;
+    public enum Motion { 
+        Idle,
+        Action,
+        Cancel
+    }
+    Motion motion;
+    //기본 상태 
+    public bool bIsIdle = true;
+    //스킬 취소 상태 여부 
+    public bool bIsCancel =false;
 
-
-    //스킬 취소 했는가 ?
-    public bool isCancel;
-
-    public GameObject rangeIndicator; // 범위 표시기 오브젝트를 할당합니다.
-    public GameObject maxRangeIndicator; // 최대 사거리 표시기 오브젝트를 할당합니다.
-    public float skillRange = 5f; // 스킬 사거리를 설정합니다.
-    public float maxSkillRange = 10f; // 스킬의 최대 사거리를 설정합니다.
+    public GameObject gRangeIndicator; // 범위 표시기 오브젝트를 할당합니다.
+    public GameObject gMaxRangeIndicator; // 최대 사거리 표시기 오브젝트를 할당합니다.
+    public GameObject gSkillCancel;
+    public float fSkillRange = 5f; // 스킬 사거리를 설정합니다.
+    public float fMaxSkillRange = 10f; // 스킬의 최대 사거리를 설정합니다.
 
 
     //일시 정지 할때 멈추기 
-    public bool isStop =false;
-
+    public bool bIsStop =false;
+   
 
 
     Vector3 Vmove;
@@ -27,8 +34,8 @@ public class Action : MonoBehaviour
     public VariableJoystick joystick;
    
     Animator ani;
-    public GameObject Bullet;
-    public Transform Bulletpos;
+    public GameObject gBullet;
+    public Transform tBulletpos;
     Stat stat;
 
     //입장 했는가 ?
@@ -45,7 +52,7 @@ public class Action : MonoBehaviour
         targetingRange = stat.fDefaultRange;
       
         // 최대 사거리 표시기 크기 설정
-        maxRangeIndicator.transform.localScale = new Vector3(maxSkillRange, maxRangeIndicator.transform.localScale.y, maxSkillRange);
+        gMaxRangeIndicator.transform.localScale = new Vector3(fMaxSkillRange, gMaxRangeIndicator.transform.localScale.y, fMaxSkillRange);
     }
 
 	// Update is called once per frame
@@ -57,10 +64,10 @@ public class Action : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (!isStop)
+            if (!bIsStop)
             {
                 Pause();
-            }else if (isStop)
+            }else if (bIsStop)
 			{
                 reStart();
 			}
@@ -77,44 +84,81 @@ public class Action : MonoBehaviour
         }
 
 
+        SkillFunction();
+      
+        
+    }
+
+   
+    void SkillFunction() {
+
+        //조이스틱 벡터
         Vector3 joystickDirection = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+        
 
         if (joystickDirection.magnitude > 0.1f)
         {
             lastJoystickDirection = joystickDirection;
             //Debug.Log(lastJoystickDirection);
             // 스킬 범위 표시
-            rangeIndicator.transform.position = transform.position + joystickDirection * 5;//5는 스킬 범위로 
-            rangeIndicator.SetActive(true); //스킬범위
-            maxRangeIndicator.SetActive(true); //스킬 사거리
+            gRangeIndicator.transform.position = transform.position + joystickDirection * 5;//5는 스킬 범위로 
+
+            gRangeIndicator.SetActive(true); //스킬범위
+            gMaxRangeIndicator.SetActive(true); //스킬 사거리
+            gSkillCancel.SetActive(true);
         }
 
-        if (joystickDirection.magnitude <= 0.1f && lastJoystickDirection.magnitude > 0.1f)
+        if (joystickDirection.magnitude <= 0.1f) {
+            gRangeIndicator.SetActive(false);
+            gMaxRangeIndicator.SetActive(false);
+            gSkillCancel.SetActive(false);
+
+        }
+        /*
+        if (bIsCancel)
         {
-            rangeIndicator.SetActive(false);
-            maxRangeIndicator.SetActive(false);
-            
-            /*
-             * 스킬 실행 취소 
-             *  onCancelEnter();
-             */
+            if (joystickDirection.magnitude <= 0.1f)
+            {
+                gRangeIndicator.SetActive(false);
+                gMaxRangeIndicator.SetActive(false);
+                gSkillCancel.SetActive(false);
+                lastJoystickDirection = Vector3.zero;
+                Debug.Log("스킬 실행 취소 ");
+                bIsIdle = true;
+                bIsCancel = false;
+            }
+          
+        } 
+        else if (!bIsCancel&& bIsIdle)
+        { 
+            if (joystickDirection.magnitude <= 0.1f)
+            { 
+                gRangeIndicator.SetActive(false);
+                gMaxRangeIndicator.SetActive(false);
+                gSkillCancel.SetActive(false);
+                
+               
+                Debug.Log("스킬실행");
+                Vector3 targetPosition = transform.position + lastJoystickDirection;
+                   
+                transform.LookAt(targetPosition);
 
-            Vector3 targetPosition = transform.position + lastJoystickDirection;
-            transform.LookAt(targetPosition);
+                //스킬 실행
 
-            Debug.Log("스킬 실행");
-
-            //스킬 실행
-            /*
-                스킬 발사 기능 구현 
-                이펙트 펙토리에서 바로 받아서 사용
-             */
+                //스킬 발사 기능 구현 
+                // 이펙트 펙토리에서 바로 받아서 사용
+                
 
 
 
-            lastJoystickDirection = Vector3.zero;
-        }
+              
+                lastJoystickDirection = Vector3.zero;
+
+            }
+        }*/
     }
+
+
 
     void onMove()
     {
@@ -139,19 +183,20 @@ public class Action : MonoBehaviour
         GameObject closestEnemy = FindClosestEnemyWithTag("Enemy", targetingRange);
         Vector3 direction;
 
+
         if (closestEnemy != null)
         {
-            direction = (closestEnemy.transform.position - Bulletpos.position).normalized;
-            GameObject bullet = Instantiate(Bullet, Bulletpos.position, Quaternion.LookRotation(direction));
+            direction = (closestEnemy.transform.position - tBulletpos.position).normalized;
+            GameObject bullet = Instantiate(gBullet, tBulletpos.position, Quaternion.LookRotation(direction));
             Rigidbody bulletrigid = bullet.GetComponent<Rigidbody>();
             transform.LookAt(direction);
             bulletrigid.velocity = direction * 50;
         }
         else if (closestEnemy == null)
         {
-            GameObject bullet = Instantiate(Bullet, Bulletpos.position, Bulletpos.rotation);
+            GameObject bullet = Instantiate(gBullet, tBulletpos.position, tBulletpos.rotation);
             Rigidbody bulletrigid = bullet.GetComponent<Rigidbody>();
-            bulletrigid.velocity = Bulletpos.forward * 50;
+            bulletrigid.velocity = tBulletpos.forward * 50;
         }
 
 
@@ -183,9 +228,10 @@ public class Action : MonoBehaviour
         return closest;
     }
 
+
     //Bool 변수 false로 바꾸는 변수 
     public void Pause() {
-        isStop = true;
+        bIsStop = true;
         ani.enabled = false;
         isEntries = false;
         Debug.Log("일시 정지");
@@ -193,7 +239,7 @@ public class Action : MonoBehaviour
     }
     public void reStart()
     {
-        isStop = false;
+        bIsStop = false;
         ani.enabled = true;
         isEntries = true;
         Debug.Log("일시 정지 해제 ");
@@ -203,10 +249,13 @@ public class Action : MonoBehaviour
 
     //이벤트 트리거에서 사용
     public void onCancelEnter() {
-        isCancel = true;
+        Debug.Log("onCancelEnter 실행");
+        motion=Motion.Cancel;
     }
     public void onCancelExit() {
-        isCancel = false;
+        Debug.Log("onCancelExit 실행 ");
+        motion = Motion.Idle;
+
     }
     
 }
