47067cbfa736b47d2f65e00661b583ce27c74129
Gkrbs <martian97@naver.com>
2023-09-18 12:01:32 +0900

    Update OrganizeChar.cs

--- Assets/Scripts/Organizing/OrganizeChar.cs
+++ Assets/Scripts/Organizing/OrganizeChar.cs
@@ -34,25 +34,47 @@ public class OrganizeChar : MonoBehaviour
         switch (index)
         {
             case 0:
-                GameManager.instance.arrCurCharacters[0] = null;
-                GameManager.instance.SetCharId(0, -1);
-                buttons[0].GetComponent<Image>().sprite
-                    = GameManager.instance.LoadAndSetSprite(FolderPath.SPRITE + "SkillImagetest.png");
+                ButtonImageSetting(0);
                 break;
             case 1:
-                GameManager.instance.arrCurCharacters[1] = null;
-                GameManager.instance.SetCharId(1, -1);
-                buttons[1].GetComponent<Image>().sprite
-                    = GameManager.instance.LoadAndSetSprite(FolderPath.SPRITE + "SkillImagetest.png");
+                ButtonImageSetting(1);
                 break;
             case 2:
-                GameManager.instance.arrCurCharacters[2] = null;
-                GameManager.instance.SetCharId(2, -1);
-                buttons[2].GetComponent<Image>().sprite
-                    = GameManager.instance.LoadAndSetSprite(FolderPath.SPRITE + "SkillImagetest.png");
+                ButtonImageSetting(2);
                 break;
             default:
                 break;
+        }
+    }
+    private void ButtonImageSetting(int index)
+    {
+        //if(index < 2)
+        //{
+        //    GameManager.instance.arrCurCharacters[index] = GameManager.instance.arrCurCharacters[index + 1];
+        //    GameManager.instance.SetCharId(index, GameManager.instance.fCharid[1]);
+        //    buttons[index].GetComponent<Image>().sprite = buttons[index + 1].GetComponent<Image>().sprite;
+        //}
+        //else
+        //{
+        //    GameManager.instance.arrCurCharacters[index] = GameManager.instance.arrCurCharacters[index + 1];
+        //    GameManager.instance.SetCharId(index, GameManager.instance.fCharid[1]);
+        //    buttons[index].GetComponent<Image>().sprite = buttons[index + 1].GetComponent<Image>().sprite;
+        //}
+
+        GameManager.instance.arrCurCharacters[index] = null;
+        GameManager.instance.SetCharId(index, -1);
+        buttons[index].GetComponent<Image>().sprite
+             = GameManager.instance.LoadAndSetSprite(FolderPath.SPRITE + "SkillImagetest.png");
+        for (int i = index + 1; i < buttons.Length; i++)
+        {
+            GameManager.instance.arrCurCharacters[i - 1] = GameManager.instance.arrCurCharacters[i];
+            GameManager.instance.SetCharId(i - 1, GameManager.instance.fCharid[i]);
+            buttons[i - 1].GetComponent<Image>().sprite = buttons[i].GetComponent<Image>().sprite;
+
+            GameManager.instance.arrCurCharacters[i] = null;
+            GameManager.instance.SetCharId(i, -1);
+            buttons[i].GetComponent<Image>().sprite 
+                = GameManager.instance.LoadAndSetSprite(FolderPath.SPRITE + "SkillImagetest.png");
         }
     }
 }
