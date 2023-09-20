bbebf5dc878234c9d607d5f08fc564dfeeb6ecad
ruddnjs1289 <71740497+ruddnjs1289@users.noreply.github.com>
2023-09-20 15:54:43 +0900

    적 이동 로직 수정
    공격 사거리  시야 사거리 수정

--- Assets/Scripts/Charactor/Bullet.cs
+++ Assets/Scripts/Charactor/Bullet.cs
@@ -6,19 +6,30 @@ public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
     public ParticleSystem particleSystem;
-	private void OnCollisionEnter(Collision collision)
-	{
-        if (collision.transform.gameObject.tag == "Enemy") {
+    public enum BulletType { 
+       ENEMY,
+       PLAYER
+    }
+    public BulletType bulletType;
 
-            ParticleSystem swpanparicle = Instantiate(particleSystem,collision.contacts[0].point, Quaternion.identity);
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (bulletType==BulletType.PLAYER&&collision.transform.gameObject.tag == "Enemy")
+        {
+
+            ParticleSystem swpanparicle = Instantiate(particleSystem, collision.contacts[0].point, Quaternion.identity);
             swpanparicle.Play();
             Destroy(swpanparicle.gameObject, swpanparicle.main.duration);
             //ÃÑ¾Ë
             Destroy(gameObject);
-            
-
-           
-
         }
-	}
+        else if (bulletType == BulletType.ENEMY && collision.transform.gameObject.tag == "Player")
+        {
+            ParticleSystem swpanparicle = Instantiate(particleSystem, collision.contacts[0].point, Quaternion.identity);
+            swpanparicle.Play();
+            Destroy(swpanparicle.gameObject, swpanparicle.main.duration);
+            //ÃÑ¾Ë
+            Destroy(gameObject);
+        }
+    }
 }
--- Assets/Scripts/Charactor/Enemy.cs
+++ Assets/Scripts/Charactor/Enemy.cs
@@ -5,147 +5,192 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    NavMeshAgent nav;
-    public GameObject target;
-    Animator ani;
+	NavMeshAgent nav;
+	public GameObject target;
+	Animator ani;
+	public GameObject AtkRange;
+	public GameObject gBullet;
 
-    bool do_attack_coroutine=false;
-
-
-    public enum State { 
-        IDLE,
-        CHASE,
-        ATTACK
-    }
-
-    public State state;
-
-    Rigidbody rigidbody;
-
-    public BoxCollider AtkRange;
-
-
-    void Start()
-    {
-        nav = GetComponent<NavMeshAgent>();
-        ani = GetComponentInChildren<Animator>();
-        rigidbody = GetComponent<Rigidbody>();
-        state = State.CHASE;
-
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (state == State.CHASE)
-        {
-
-            target = GameObject.FindWithTag("Player");
-            nav.SetDestination(target.transform.position);
-            ani.SetBool("doWalk", true);
-        }
-
-        if (target == null)
-        {
-            Debug.Log("플레이어 없음  없음");
-            state = State.IDLE;
-        }
-
-
-      
-         
-        
-       
-        
-
-
-
-        /*
-        //멈추면 공격
-        if (Vector3.Distance(target.transform.position, transform.position) <= nav.stoppingDistance)
-            {
-                ani.SetBool("doWalk", false);
-                //바로 멈추기 
-                nav.SetDestination(transform.position);
-                transform.LookAt(target.transform.position);
-                ani.SetBool("doAttack", true);
-            }
-            //아니면 이동 
-            else
-            {
-                ani.SetBool("doWalk", true);
-                ani.SetBool("doAttack", false);
-                nav.SetDestination(target.transform.position);
-                ani.SetBool("doAttack", false);
-            }*/
-    }
-
-    private void FixedUpdate()
-    {
-        Targeting();
-        FreezeVelocity();
-    }
-    void Targeting()
-	{
-        float targetRadius = 2f;
-        float tartgeRange = 2f;
-        
-
-        RaycastHit[] rayHits = Physics.SphereCastAll(transform.position, targetRadius, transform.forward, tartgeRange, LayerMask.GetMask("Player"));
-        Debug.Log(rayHits.Length);
-
-        if (rayHits.Length > 0f && state != State.ATTACK)
-        {
-            //방어 코드  이유 : 중복 실행 방지
-            if(!do_attack_coroutine)
-            StartCoroutine("Attack");
-        }
-        else if(rayHits.Length == 0) {
-            state = State.CHASE;
-            ani.SetBool("doWalk",true);
-            ani.SetBool("doAttack", false);
-        }
-      
-        
-      
+	public bool do_attack_coroutine = false;
+	public enum MonsterType { 
+		Melee,
+		Range,
+		Boss
 	}
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, 1f);
-    }
+	public MonsterType monsterType;
+
+	public enum State
+	{
+		IDLE,
+		CHASE,
+		ATTACK
+	}
+
+	public State state;
+
+	Rigidbody rigidbody;
+
+	
+
+
+	void Start()
+	{
+		nav = GetComponent<NavMeshAgent>();
+		ani = GetComponentInChildren<Animator>();
+		rigidbody = GetComponent<Rigidbody>();
+		state = State.IDLE;
+
+	}
+
+
+	// Update is called once per frame
+	/*  void Update()
+	  {
+		  target = GameObject.FindWithTag("Player");
+
+		  if (state == State.CHASE)
+		  { 
+			  nav.SetDestination(target.transform.position);
+			  ani.SetBool("doWalk", true);
+		  }
+
+		  if (target == null)
+		  {
+			  Debug.Log("플레이어 없음  없음");
+			  state = State.IDLE;
+		  }
+
+	  }*/
+
+	private void FixedUpdate()
+	{
+		target = GameObject.FindWithTag("Player");
+		FreezeVelocity();
+		Search();
+		Targeting();
+
+
+	}
+	
+	//시야 사거리안에 적 찾아서   추적하기 
+	void Search()
+	{
+		float targetRadius = 20f;// 적의 사거리로  바꾸기
+		float targetRange = 0;// 적의 사거리로 바꾸기 
+		RaycastHit[] rayHits = Physics.SphereCastAll(transform.position, targetRadius, transform.forward, targetRange, LayerMask.GetMask("Player"));
+
+		// Chase 상태일때 search가 작동안함
+		// Chase 상태이면 else if 문을 타서
+		// nav.Setdestination이 자기 자신의 위치가 찍힘
+		// 상태는 IDLE임? -> 이건 모르겠음
+		
+		if (rayHits.Length > 0 && state == State.IDLE)
+		{
+			Debug.Log("시야 사거리 적 서치");
+			ani.SetBool("doWalk", true);
+
+			state = State.CHASE;
+			nav.isStopped = false;
+			nav.SetDestination(target.transform.position);
+		}
+		if (rayHits.Length > 0 && state == State.CHASE)
+		{
+			nav.SetDestination(target.transform.position);
+			nav.isStopped = false;
+		}
+		else if (rayHits.Length == 0)
+		{
+			ani.SetBool("doAttack", false);
+			ani.SetBool("doWalk", false);
+			nav.isStopped = true;
+			state = State.IDLE;
+		}
+
+	}
+
+	// 공격사거리안의 적 찾아서 적 공격하기 
+	void Targeting()
+	{
+		float targetRadius = 10f;// 적의 사거리로  바꾸기
+		float targetRange = 0;// 적의 사거리로 바꾸기 
+
+
+		RaycastHit[] rayHits = Physics.SphereCastAll(transform.position, targetRadius, transform.forward, targetRange, LayerMask.GetMask("Player"));
+		Debug.Log(rayHits.Length);
+
+		if (rayHits.Length > 0f && state != State.ATTACK)
+		{
+			//방어 코드  이유 : 중복 실행 방지
+			if (!do_attack_coroutine)
+				StartCoroutine("Attack");
+			nav.SetDestination(transform.position);
+		}
+		else if (rayHits.Length == 0 && state == State.ATTACK)
+		{
+
+			ani.SetBool("doAttack", false);
+			ani.SetBool("doWalk", true);
+			nav.SetDestination(target.transform.position);
+			state = State.CHASE;
+		}
 
 
 
-    IEnumerator Attack() {
-        do_attack_coroutine = true;
-        ani.SetBool("doWalk", false);
-        nav.SetDestination(transform.position);
-        transform.LookAt(target.transform.position);
-        ani.SetBool("doAttack", true);
-        state = State.ATTACK;
-        while (true)
-        {
-            if (state != State.ATTACK) break;
-                yield return new WaitForSeconds(0.2f);
-                AtkRange.enabled = true;
-                yield return new WaitForSeconds(ani.GetCurrentAnimatorStateInfo(0).length);
-                AtkRange.enabled = false;
-        }
+	}
+	private void OnDrawGizmos()
+	{
 
-        do_attack_coroutine = false;
+		//시야 사거리
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, 20);
 
 
+		//공격 사거리
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(transform.position, 10);
+	}
 
-    }
-    void FreezeVelocity()
-    {
-        // navagent 할때 물리력 제외 
-        if (nav.enabled)
-        {
-            rigidbody.angularVelocity = Vector3.zero;
-            rigidbody.velocity = Vector3.zero;
-        }
-    }
+	IEnumerator Attack()
+	{
+		Vector3 distance = target.transform.position - AtkRange.transform.position;
+		do_attack_coroutine = true;
+		ani.SetBool("doWalk", false);
+		transform.LookAt(target.transform.position);
+		ani.SetBool("doAttack", true);
+		state = State.ATTACK;
+		while (true)
+		{
+			
+			if (state != State.ATTACK)
+			{
+				do_attack_coroutine = false;
+				
+				yield break;
+			}
+			yield return new WaitForSeconds(0.2f);
+			AtkRange.SetActive(true);
+			GameObject newBullet = Instantiate(gBullet, AtkRange.transform.position, AtkRange.transform.rotation);
+			Rigidbody bulletrigid = newBullet.GetComponent<Rigidbody>();
+			
+			bulletrigid.velocity = target.transform.position * 3;
+			yield return new WaitForSeconds(ani.GetCurrentAnimatorStateInfo(0).length);
+			AtkRange.SetActive(false);
+		}
+
+
+
+
+
+
+	}
+	void FreezeVelocity()
+	{
+		// navagent 할때 물리력 제외 
+		if (nav.enabled)
+		{
+			rigidbody.angularVelocity = Vector3.zero;
+			rigidbody.velocity = Vector3.zero;
+			rigidbody.freezeRotation = true;
+		}
+	}
 }
