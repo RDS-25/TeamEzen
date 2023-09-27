6e9ec258e183947e1961dffbaa4803548ea25918
Gkrbs <martian97@naver.com>
2023-09-27 15:36:16 +0900

    스테이지 패널
    스테이지 매니저 연결 필요

--- Assets/Scripts/SkillPanel/SkillLevelUpUi.cs
+++ Assets/Scripts/SkillPanel/SkillLevelUpUi.cs
@@ -13,21 +13,33 @@ public class SkillLevelUpUi : MonoBehaviour
     //public Image imgFirstMaterial;
     //public Image imgSecondMaterial;
     //public Image imgThirdMaterial;
-
+    public Dictionary<float, float> dictMatExp = new();
+    Dictionary<string, string> dictItemCount = new();
 
     public List<Button> listButtons = new();
+    public List<Button> listMatButtons = new();
     public SkillPanelUi skillPanelUi;
     Dictionary<string, string> dictSelectedSkillParams = new();
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < listButtons.Count; i++)
+        dictItemCount = GameManager.instance.DataRead(FolderPath.PARAMS_ITEM_COUNT);
+
+        for (int i = 0; i < listButtons.Count; i++)
         {
             int buttonIndex = i;
             listButtons[i].onClick.AddListener(() => OnClickButton(buttonIndex));
         }
+        for (int i = 0; i < listButtons.Count; i++)
+        {
+            int buttonIndex = i;
+            listButtons[i].onClick.AddListener(() => OnClickMatExpButton(buttonIndex));
+        }
     }
-
+    void Update()
+    {
+        // dictMatExp 여기에 들어있는 id 값을 가진 머테리얼 표시해주기
+    }
     void OnClickButton(int index)
     {
         switch (index)
@@ -52,6 +64,15 @@ public class SkillLevelUpUi : MonoBehaviour
                 break;
         }
     }
+    void OnClickMatExpButton(int index)
+    {
+        // 선택한 재료 딕셔너리에 개수 저장
+        index += 300;
+        if (!dictMatExp.ContainsKey(index))
+            dictMatExp.Add(index, 0);
+        dictMatExp[index] += 1;
+    }
+
     void SetDictSkillParams(Dictionary<string,string> dictTemp)
     {
         dictSelectedSkillParams = dictTemp;
@@ -64,8 +85,8 @@ public class SkillLevelUpUi : MonoBehaviour
             (FolderPath.SPRITE_SKILL_ICON + dictSelectedSkillParams[SkillID.ICON_NAME]);
         skillName.text = dictSelectedSkillParams[SkillID.NAME];
         skillDescription.text = dictSelectedSkillParams[SkillID.DESCRIPT];
-
     }
+
     public void LevelUpSkill()
     {
         foreach(GameObject skills in GameManager.instance.objectFactory.AllSkill.listPool)
@@ -76,15 +97,26 @@ public class SkillLevelUpUi : MonoBehaviour
                 if(skillScript.fSkillLevel < 10)
                 {
                     skillScript.SkillActivationInit(ref skillPanelUi.curCharStat);
-                    skillScript.SkillLevelUp();
-                    //dictSelectedSkillParams = GameManager.instance.DataRead(skillScript.strSkillFolderPath + skillScript.strSkillParamsName);
+                    skillScript.SkillExpUp(SelectMaterialToExpUp());
                     ShowSkillData();
                     skillPanelUi.ShowSkill();
                     if(skillScript.fSkillLevel == 10)
                         nextLevel.text = "Max";
                 }
+                break;
             }
         }
-        //currentLevel.text = "Lv. " + dictSelectedSkillParams[SkillID.LEVEL] + 1;
+    }
+    float SelectMaterialToExpUp()
+    {
+        float fSumExp = 0;
+        foreach (KeyValuePair<float, float> MatExpPair in dictMatExp)
+        {
+            fSumExp = MatExpPair.Key * MatExpPair.Value;
+            // 재료 선택한 딕셔너리 초기화해주고 itemcount 빼주기 만들기
+            dictItemCount[MatExpPair.Key.ToString()]
+                = (int.Parse(dictItemCount[MatExpPair.Key.ToString()]) - MatExpPair.Value).ToString();
+        }
+        return fSumExp;
     }
 }
