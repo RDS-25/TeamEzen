227ce0261a9e827ffe5b64659349a454096e07e6
ruddnjs1289 <71740497+ruddnjs1289@users.noreply.github.com>
2023-10-05 15:57:29 +0900

    사거리 표현

--- Assets/Scripts/Charactor/OnClickSkills.cs
+++ Assets/Scripts/Charactor/OnClickSkills.cs
@@ -8,46 +8,52 @@ public class OnClickSkills : MonoBehaviour
 
 	public List<GameObject> BtnSKills;
 
+	public GameObject BtnPrev;//이전
+
 	public void ClickSkills() {
+
+		
 		GameObject Skill = EventSystem.current.currentSelectedGameObject;
 		var scSKill = Skill.GetComponent<SkillTest>();
-
-
-		if (scSKill.sKillType == SkillTest.SKillType.Ranged) {
-			
-			scSKill.targetCircle.enabled = true;
-			scSKill.indicatorRangeCircle.enabled = true;
-			//범위 표시기 크기 지정 
-			float maxDistacne = scSKill.maxActiveSkillDistance * 2f;
-			//스킬 범위 크기 지정
-			float targetSize = scSKill.targetCircleSize * 2f;
-
-			scSKill.indicatorRangeCircle.GetComponent<RectTransform>().sizeDelta = new Vector2(maxDistacne, maxDistacne);
-			scSKill.targetCircle.GetComponent<RectTransform>().sizeDelta = new Vector2(targetSize, targetSize);
-
-		}else if (scSKill.sKillType == SkillTest.SKillType.Arrow)
+		
+		scSKill.enabled = true;
+		Skill.GetComponent<SkillTest>().isSkilling = true;
+		/*Skill.GetComponent<SkillTest>().Skillstate = SkillTest.SkillState.Clicked;*/
+		if(Skill != BtnPrev)
 		{
-			
-			scSKill.ABCImage.enabled = true;
+			scSKill.isCancel = true;
 		}
+			if (scSKill.sKillType == SkillTest.SKillType.Ranged)
+			{
 
+				scSKill.targetCircle.enabled = true;
+				scSKill.indicatorRangeCircle.enabled = true;
+				scSKill.ABCImage.enabled = false;
+				//범위 표시기 크기 지정 
+				float maxDistacne = scSKill.maxActiveSkillDistance * 2f;
+				//스킬 범위 크기 지정
+				float targetSize = scSKill.targetCircleSize * 2f;
+
+				scSKill.indicatorRangeCircle.GetComponent<RectTransform>().sizeDelta = new Vector2(maxDistacne, maxDistacne);
+				scSKill.targetCircle.GetComponent<RectTransform>().sizeDelta = new Vector2(targetSize, targetSize);
+
+			}
+			else if (scSKill.sKillType == SkillTest.SKillType.Arrow)
+			{
+				scSKill.ABCImage.enabled = true;
+				scSKill.targetCircle.enabled = false;
+				scSKill.indicatorRangeCircle.enabled = false;
+			}
+		
 
 		foreach (GameObject btn in BtnSKills) {
 			if (btn != Skill )
 			{
 				btn.GetComponent<SkillTest>().enabled = false;
-
-				if (btn.GetComponent<SkillTest>().sKillType == SkillTest.SKillType.Ranged) {
-					btn.GetComponent<SkillTest>().ABCImage.enabled = false;
-				}
-				else if (btn.GetComponent<SkillTest>().sKillType == SkillTest.SKillType.Arrow)
-				{
-					btn.GetComponent<SkillTest>().ABCImage.enabled = true;
-					btn.GetComponent<SkillTest>().targetCircle.enabled = false;
-					btn.GetComponent<SkillTest>().indicatorRangeCircle.enabled = false;
-				}
 			}
-		
+	
 		}
+
+		BtnPrev = Skill;
 	}
 }
--- Assets/Scripts/Charactor/SkillTest.cs
+++ Assets/Scripts/Charactor/SkillTest.cs
@@ -13,33 +13,43 @@ public class SkillTest : MonoBehaviour
     public Vector3 Pos; //마우스 포인터 위치 
     public float maxActiveSkillDistance; //최대사거리  
     public float targetCircleSize;
+    public GameObject TestOBJ;
+   
+    public Camera cam;
 
     public enum SKillType {Arrow,Ranged }
     public SKillType sKillType;
+
+    public enum SkillState {Init,Clicked,Active,}
+    public SkillState Skillstate;
     GameObject Player;
+    //스킬 사용중?
+    public bool isSkilling;
+    //스킬 캔슬
+    public bool isCancel;
 
 
 
     void Start()
     {
-        targetCircle.GetComponent<Image>().enabled = false;
-        indicatorRangeCircle.GetComponent<Image>().enabled = false;
-        ABCImage.GetComponent<Image>().enabled = false;
-        //Gameobj instance ~~~~ 스킬 인스턴스 사거리 값
+        
         Player = GameObject.FindWithTag("Player");
+        cam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-     
+   
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin,ray.direction ,Color.yellow);
 
         if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
             Pos = new Vector3(hit.point.x,hit.point.y,hit.point.z);
         }
+
         if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
             if (hit.collider.gameObject != this.gameObject) {
                 Pos = hit.point;
@@ -59,7 +69,31 @@ public class SkillTest : MonoBehaviour
         var newHitPos = Player.transform.position  + hitPosDir * distance;
 
         ActiveSkillCanvas.transform.position = newHitPos;
+        
+        //Arrow와 Range의 나가는 스킬이 다르다 
+        //Arrow는  발사체  Range는 소환
 
+        //버튼 누를때 실행 안되게 and 처음에 실행 안되게 
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("겟마우스 다운 실행");
+			if (isCancel)
+			{
+                isCancel = !isCancel;
+                return;
+			}
+            Instantiate(TestOBJ, newHitPos, TestOBJ.transform.rotation);
+            isSkilling = false;
+            AllImageFalse();
+            Debug.Log(hit.collider.gameObject);
+        }
+
+    }
+
+    void AllImageFalse() {
+
+        targetCircle.GetComponent<Image>().enabled = false;
+        indicatorRangeCircle.GetComponent<Image>().enabled = false;
+        ABCImage.GetComponent<Image>().enabled = false;
     }
 
 
